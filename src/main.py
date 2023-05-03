46d8d7afa0a1cc9c978c0f897bcfe926fb57ef20
Nils Van den Steen <nvandensteen@naturalsciences.be>
2023-05-03 21:49:36 +0200

    [WIP] trying to understand API

--- src/main.py
+++ src/main.py
@@ -4,6 +4,15 @@ from stapy import Query, Entity
 import json
 
 
+def get_names(query:Query) -> list[str]:
+    names = query.select("name").get_data_sets()
+    return names
+
+
+def inspect_datastreams_thing(entity_id: int) -> dict[str | dict[str]]:
+    pass
+
+
 @hydra.main(config_path="../conf", config_name="config.yaml", version_base="1.2")
 def main(cfg):
     stapy.set_sta_url(cfg.data_api.base_url)
@@ -15,29 +24,53 @@ def main(cfg):
     # # for t in things:
     # #     print(f"{t=}")
 
-    t_expand = Query(Entity.Thing).expand("Datastreams")
-    ds = t_expand.get_data_sets(query=t_expand.select("@iot.id").get_query())
-    print(f"{ds=}")
-    a = t_expand.get_with_retry(t_expand.get_query())
-    data = json.loads(a.content)
-    payload = data.get("value")
+    t_expand = Query(Entity.Thing).entity_id(1)
+    print(f"{t_expand.get_query()=}")
+    names_datastreams = get_names(t_expand.sub_entity(Entity.Datastream))
+    print(f"{t_expand.sub_entity(Entity.Datastream).get_query()=}")
+    q_observedProperties = t_expand.sub_entity(Entity.Datastream).select("ObservedProperty@iot.navigationLink")
+    print(f"{q_observedProperties.get_query()=}")
 
-    print(f"{type(data)=}")
-    print(f"{type(payload)=}")
-    print(f"{len(payload)=}")
-    for pi in payload:
-        print(f"{pi.keys()=}")
-        print(f"{pi.get('@iot.id')=} : {pi.get('name', '')=}")
-        print(f"{len(pi.get('Datastreams'))=}")
-    # print(f"{a=}")
 
-    d_expand_obs = Query(Entity.Datastream).entity_id(5).expand(Entity.Observation)
-    payload = json.loads(d_expand_obs.get_with_retry(d_expand_obs.get_query()).content).get("value)")
 
-    # print(f"{len(payload)}")
+    a0 = Query(Entity.Thing).entity_id(1).expand(Entity.Datastream.value)
+    a1 = a0.sub_entity(Entity.Datastream)
+    a2 = a1.expand(Entity.ObservedProperty.value)
+    print(f"{a0.get_query()}")
+    print(f"{a1.get_query()}")
+    print(f"{a2.get_query()}")
+    # a = Query(Entity.Thing).entity_id(1).expand(Entity.Datastream.value).expand(Entity.ObservedProperty.value).select("ObservedProptery")
 
-    # t_expand.expand()
-    # print(f"{t_expand=}")
+    # print(f"{a.get_query()=}")
+    # print(f"{q_observedProperties.get_query()=}")
+    # print(f"{t_expand.get_query()=}")
+    print(f"{len(names_datastreams)=}")
+    print(f"{len(set(names_datastreams))=}")
+    # print(f"{names_observedProperties=}")
+    # print(f"{len(names_observedProperties)=}")
+    # print(f"{len(set(names_observedProperties))=}")
+    ## ds = t_expand.get_data_sets(query=t_expand.select("@iot.id").get_query())
+    ## print(f"{ds=}")
+    ## a = t_expand.get_with_retry(t_expand.get_query())
+    ## data = json.loads(a.content)
+    ## payload = data.get("value")
+
+    ## print(f"{type(data)=}")
+    ## print(f"{type(payload)=}")
+    ## print(f"{len(payload)=}")
+    ## for pi in payload:
+    ##     print(f"{pi.keys()=}")
+    ##     print(f"{pi.get('@iot.id')=} : {pi.get('name', '')=}")
+    ##     print(f"{len(pi.get('Datastreams'))=}")
+    ## # print(f"{a=}")
+
+    ## d_expand_obs = Query(Entity.Datastream).entity_id(5).expand(Entity.Observation)
+    ## payload = json.loads(d_expand_obs.get_with_retry(d_expand_obs.get_query()).content).get("value)")
+
+    ## # print(f"{len(payload)}")
+
+    ## # t_expand.expand()
+    ## # print(f"{t_expand=}")
 
 
 if __name__ == "__main__":
