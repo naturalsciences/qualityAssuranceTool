[FIX] range/gradient through bool

src/services/qc.py
@@ -36,7 +36,9 @@ def get_bool_out_of_range(
     #     qc_type_min = (qc_type_max, qc_on[1])
     #     qc_type_max= (qc_type_min, qc_on[1])
 
-    s_bool_out = (df[qc_on] <= df[qc_type_max]) & (df[qc_on] >= df[qc_type_min])
+    s_bool_out = ((df[qc_on] > df[qc_type_max]) & ~df[qc_type_max].isnull()) | (
+        (df[qc_on] < df[qc_type_min]) & ~df[qc_type_min].isnull()
+    )
     return s_bool_out
 
 
@@ -45,8 +47,8 @@ def qc_df(df_in, function):
     # http://vocab.nerc.ac.uk/collection/L20/current/
     df_out = deepcopy(df_in)
     df_out["bool"] = function(df_out[Df.RESULT].array)
-    df_out.loc[df_out["bool"], Df.QC_FLAG] = QualityFlags.PROBABLY_GOOD
-    df_out.loc[~df_out["bool"], Df.QC_FLAG] = QualityFlags.PROBABLY_BAD
+    df_out.loc[~df_out["bool"], Df.QC_FLAG] = QualityFlags.PROBABLY_GOOD
+    df_out.loc[df_out["bool"], Df.QC_FLAG] = QualityFlags.PROBABLY_BAD
     return df_out
 
 
@@ -71,15 +73,35 @@ def qc_df(df_in, function):
 #     return df_out
 
 
+def get_bool_null_region(df: pd.DataFrame) -> pd.Series:
+    return df[Df.REGION].isnull()
+
+
+def get_bool_land_region(df: pd.DataFrame) -> pd.Series:
+    bool_mainland = df[Df.REGION].str.lower().str.contains("mainland").fillna(False)  # type: ignore
+    return bool_mainland
+
+
 def qc_region(df: gpd.GeoDataFrame) -> gpd.GeoDataFrame:
     df_out = deepcopy(df)
 
-    bool_nan = df_out[Df.REGION].isnull()  # type: ignore
-    df_out.loc[bool_nan, Df.QC_FLAG] = QualityFlags.PROBABLY_BAD  # type: ignore
+    bool_nan = get_bool_null_region(df_out)
+    df_out.loc[bool_nan.index, Df.QC_FLAG] = get_qc_flag_from_bool(
+        df=df_out.loc[bool_nan.index],
+        bool_=bool_nan,
+        flag_on_true=QualityFlags.PROBABLY_BAD,
+        update_verified=False,
+    )[Df.QC_FLAG]
 
-    bool_mainland = df_out[Df.REGION].str.lower().str.contains("mainland").fillna(False)  # type: ignore
-    df_out.loc[bool_mainland, Df.QC_FLAG] = QualityFlags.BAD  # type: ignore
+    bool_mainland = get_bool_land_region(df_out)
+    df_out.loc[bool_mainland.index, Df.QC_FLAG] = get_qc_flag_from_bool(
+        df=df_out.loc[bool_mainland.index],
+        bool_=bool_mainland,
+        flag_on_true=QualityFlags.BAD,
+        update_verified=False,
+    )[Df.QC_FLAG]
 
+    df_out[Df.QC_FLAG] = df_out[Df.QC_FLAG].astype(CAT_TYPE)  # type: ignore
     log.info(f"Flags set: {df_out.loc[bool_mainland | bool_nan, [Df.QC_FLAG, Df.REGION]].value_counts(dropna=False)}")  # type: ignore
     return df_out
 
@@ -101,18 +123,40 @@ def calc_gradient_results(df: pd.DataFrame, groupby: Df):
 
 
 def dependent_quantity_merge_asof(df: pd.DataFrame, independent, dependent):
-    df_indep = df.loc[df[Df.DATASTREAM_ID] == independent].sort_values(Df.TIME).set_index(Df.TIME)
-    df_dep = df.loc[df[Df.DATASTREAM_ID] == dependent].sort_values(Df.TIME).set_index(Df.TIME)
+    df_indep = (
+        df.loc[df[Df.DATASTREAM_ID] == independent]
+        .sort_values(Df.TIME)
+        .set_index(Df.TIME)
+    )
+    df_dep = (
+        df.loc[df[Df.DATASTREAM_ID] == dependent]
+        .sort_values(Df.TIME)
+        .set_index(Df.TIME)
+    )
 
     # df_merged = pd.merge_asof(df_indep, df_dep, left_index=True, right_index=True, tolerance=pd.Timedelta('0.5s'), suffixes=[f"_{i}" for i in [independent, dependent]])
-    df_merged = pd.merge_asof(df_dep, df_indep, left_index=True, right_index=True, tolerance=pd.Timedelta('0.5s'), suffixes=[f"_{i}" for i in [dependent, independent]])
-    df_merged = pd.DataFrame(df_merged.values, index=df_merged.index, columns=df_merged.columns.str.rsplit("_", expand=True, n=1))
+    df_merged = pd.merge_asof(
+        df_dep,
+        df_indep,
+        left_index=True,
+        right_index=True,
+        tolerance=pd.Timedelta("0.5s"),
+        suffixes=[f"_{i}" for i in [dependent, independent]],
+    )
+    df_merged = pd.DataFrame(
+        df_merged.values,
+        index=df_merged.index,
+        columns=df_merged.columns.str.rsplit("_", expand=True, n=1),
+    )
 
     return df_merged
 
+
 def dependent_quantity_pivot(df: pd.DataFrame, independent, dependent):
     # merge_asof is used, but creates a pivot-like table
-    df_merged = dependent_quantity_merge_asof(df, independent=independent, dependent=dependent)
+    df_merged = dependent_quantity_merge_asof(
+        df, independent=independent, dependent=dependent
+    )
     return df_merged
 
 
@@ -138,7 +182,9 @@ def qc_dependent_quantity_base(df: pd.DataFrame, independent: int, dependent: in
         df, independent=independent, dependent=dependent
     )
 
-    df_pivot = dependent_quantity_pivot(df_tmp, independent=independent, dependent=dependent)
+    df_pivot = dependent_quantity_pivot(
+        df_tmp, independent=independent, dependent=dependent
+    )
 
     mask = ~df_pivot[Df.QC_FLAG, str(independent)].isin(
         [QualityFlags.NO_QUALITY_CONTROL, QualityFlags.GOOD]
@@ -150,7 +196,7 @@ def qc_dependent_quantity_base(df: pd.DataFrame, independent: int, dependent: in
     df_unpivot = df_pivot.stack().reset_index().set_index(Df.IOT_ID)
     df = df.set_index(Df.IOT_ID)
     df.loc[df_unpivot.index, Df.QC_FLAG] = df_unpivot[Df.QC_FLAG]
-    df.loc[df[Df.QC_FLAG].isna(), Df.QC_FLAG] = QualityFlags.BAD # type: ignore
+    df.loc[df[Df.QC_FLAG].isna(), Df.QC_FLAG] = QualityFlags.BAD  # type: ignore
     return df.reset_index()
 
 
@@ -161,13 +207,15 @@ def qc_dependent_quantity_secondary(
         df, independent=independent, dependent=dependent
     )
 
-    df_pivot = dependent_quantity_pivot(df_tmp, dependent=dependent, independent=independent)
+    df_pivot = dependent_quantity_pivot(
+        df_tmp, dependent=dependent, independent=independent
+    )
 
     df_pivot[["qc_drange_min", "qc_drange_max"]] = range_
     bool_qc = get_bool_out_of_range(
         df_pivot, (Df.RESULT, str(independent)), qc_type="drange"
     )
-    df_pivot.loc[~bool_qc, (Df.QC_FLAG, str(dependent))] = QualityFlags.BAD  # type: ignore Don"t know how to fix this
+    df_pivot.loc[bool_qc, (Df.QC_FLAG, str(dependent))] = QualityFlags.BAD  # type: ignore Don"t know how to fix this
 
     df_pivot = df_pivot.drop(["qc_drange_min", "qc_drange_max"], axis=1)
     df_unpivot = df_pivot.stack().reset_index().set_index(Df.IOT_ID)
@@ -185,11 +233,12 @@ def get_qc_flag_from_bool(
     df.loc[bool_.index, Df.VERIFIED] = True
     df[Df.VALID] = (df.get(Df.VALID, True) & bool_) | ~df[Df.VERIFIED].astype(bool)  # type: ignore
 
-
-    df.loc[(df[Df.QC_FLAG] < flag_on_true), Df.QC_FLAG] = QualityFlags(flag_on_true)  # type: ignore
+    df.loc[bool_ & (df[Df.QC_FLAG] < QualityFlags(flag_on_true)), Df.QC_FLAG] = QualityFlags(flag_on_true)  # type: ignore
     df[Df.QC_FLAG] = df[Df.QC_FLAG].astype(CAT_TYPE)
 
-    columns_out = list(compress([Df.QC_FLAG, Df.VALID, Df.VERIFIED], [True, True, update_verified]))
+    columns_out = list(
+        compress([Df.QC_FLAG, Df.VALID, Df.VERIFIED], [True, True, update_verified])
+    )
     return df[columns_out]
 
 
@@ -199,10 +248,17 @@ def set_qc_flag_range_check(
 ) -> pd.DataFrame:
     df_out = deepcopy(df)
     df_out[Df.QC_FLAG] = df_out[Df.QC_FLAG].astype(CAT_TYPE)
-    mask = get_null_mask(df_out, qc_type)
-    bool_tmp = get_bool_out_of_range(df_out.loc[mask], qc_on=qc_on, qc_type=qc_type)
+    # mask = get_null_mask(df_out, qc_type)
+    # bool_tmp = get_bool_out_of_range(df_out.loc[mask], qc_on=qc_on, qc_type=qc_type)
+    bool_tmp = get_bool_out_of_range(df_out, qc_on=qc_on, qc_type=qc_type)
 
-    df_tmp = get_qc_flag_from_bool(df_out.loc[mask], bool_=bool_tmp, flag_on_true=QualityFlags.BAD, update_verified=True)
+    df_tmp = get_qc_flag_from_bool(
+        # df_out.loc[mask],
+        df_out,
+        bool_=bool_tmp,
+        flag_on_true=QualityFlags.BAD,
+        update_verified=True,
+    )
     df_out.loc[df_tmp.index, df_tmp.columns] = df_tmp
 
     return df_out
@@ -218,12 +274,12 @@ def get_bool_spacial_outlier_compared_to_median(
         .rolling(time_window, on=Df.TIME)
         .apply(np.median)
     )
-    ref_point = gpd.GeoDataFrame(
+    ref_point = gpd.GeoDataFrame(  # type: ignore
         rolling_median,
         geometry=gpd.points_from_xy(
             rolling_median.loc[:, Df.LONG], rolling_median.loc[:, Df.LAT]
         ),
-    ).set_crs("EPSG:4326")  # type: ignore
+    ).set_crs("EPSG:4326")
     bool_series = (
         df.sort_values(Df.TIME)
         .loc[:, "geometry"]