0d81c6f7c436c15cd44b203c24a900cea5d8aae9
Nils Van den Steen <nvandensteen@naturalsciences.be>
2024-01-31 21:43:28 +0100

    [UPD] small changes

--- docker_build.sh
+++ docker_build.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-docker build --network=docker_default --no-cache --build-arg ID_U=$(id -u) -t qc_sensorthings .
+docker build --no-cache -t vdsnils/quality_assurance_tool:latest .
