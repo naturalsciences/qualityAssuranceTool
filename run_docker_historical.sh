c9584bedd247a129f752392e6f3e0c0f2c91984c
Nils Van den Steen <nvandensteen@naturalsciences.be>
2024-02-12 18:36:29 +0100

    [UPD,ENH] script flagging historical data

--- run_docker_historical.sh
+++ run_docker_historical.sh
@@ -15,12 +15,18 @@ DT_UNIT=$(printf '%s\n' "${DT//[[:digit:]]/}")
 
 FMT="+%Y-%m-%d %H:%M"
 
-TEST="-$((DT_INT*DH_COUNTER)) $DT_UNIT"
-START_I=$(date --date=now"-$((DT_INT*DH_COUNTER))$DT_UNIT" "$FMT")
-END_I=$(date --date="$START_I-$((DT_INT))$DT_UNIT" "$FMT")
+START_I=$(date -u --date="$START_TIME UTC +$((DT_INT*DH_COUNTER))$DT_UNIT" "$FMT")
+END_I=$(date -u --date="$START_I UTC +$((DT_INT))$DT_UNIT" "$FMT")
 
-echo $START_I "---->" $END_I
-# docker run -it --network=host --user "$(id -u):$(id -g)" --workdir /app -v ./conf:/app/conf -v ./outputs:/app/outputs -e DEV_SENSORS_USER=$DEV_SENSORS_USER -e DEV_SENSORS_PASS=$DEV_SENSORS_PASS vdsnils/quality_assurance_tool:v0.3 "time.start=$(date --date=$now-'16minutes' +'%Y-%m-%d %H:%M')" "time.end=$(date --date=$now-'1minute' +'%Y-%m-%d %H:%M')"
+docker run \
+    --rm -d --network=host --user "$(id -u):$(id -g)"\
+    --workdir /app \
+    -v $CONFIG_FOLDER:/app/conf \
+    -v $OUTPUT_FOLDER:/app/outputs \
+    -e DEV_SENSORS_USER=$(keyctl print $(keyctl search @u user SENSORS_USER)) \
+    -e DEV_SENSORS_PASS=$(keyctl print $(keyctl search @u user SENSORS_PASS)) \
+    nbmdc/quality_assurance_tool:tmp \
+    "time.start=$START_I" "time.end=$END_I"
+
 DH_COUNTER=$((DH_COUNTER+1))
-
 echo $DH_COUNTER > counter.txt
