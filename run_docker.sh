e406e4f3f4ea7d62a464ec987af2b418914a9d10
Nils Van den Steen <nvandensteen@naturalsciences.be>
2024-02-16 18:00:50 +0100

    [DEL] unneeded files (I hope)

--- run_docker.sh
+++
@@ -1,4 +0,0 @@
-PATH=$PATH:/usr/bin
-
-cd /home/nvadmin/Documents/ODANext/qc_through_sensorthings
-docker run -it --network=host --user "$(id -u):$(id -g)" --workdir /app -v ./conf:/app/conf -v ./outputs:/app/outputs -e DEV_SENSORS_USER=$DEV_SENSORS_USER -e DEV_SENSORS_PASS=$DEV_SENSORS_PASS vdsnils/quality_assurance_tool:v0.3 "time.start=$(date --date=$now-'16minutes' +'%Y-%m-%d %H:%M')" "time.end=$(date --date=$now-'1minute' +'%Y-%m-%d %H:%M')"
--- systemd_user/qat.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-#
-
-touch /tmp/testing_now
-
-docker run -d --network=host --user "$(id -u):$(id -g)" --workdir /app -v ./conf:/app/conf -v ./outputs:/app/outputs -e HYDRA_FULL_ERROR=1 -e DEV_SENSORS_USER=$DEV_SENSORS_USER -e DEV_SENSORS_PASS=$DEV_SENSORS_PASS vdsnils/quality_assurance_tool:test "time.start=$(date --date=$now-'16minutes' +'%Y-%m-%d %H:%M')" "time.end=$(date --date=$now-'1minute' +'%Y-%m-%d %H:%M')" >> /tmp/outputtesting 2>&1
