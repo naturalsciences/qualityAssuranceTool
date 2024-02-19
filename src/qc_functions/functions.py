58abc35f292dd789c128e5cf54fa9a28b839cbd4
nvandensteen <nvandensteen@naturalsciences.be>
2024-02-19 11:37:06 +0100

    [CLN] remove unneeded files

--- src/qc_functions/functions.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-import pandas as pd
-
-
-def min_max_check_values(values: pd.DataFrame, min_: float, max_: float):
-    out = np.logical_and(values >= min_, values <= max_)
-    return out
