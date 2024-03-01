0d9f14508e559939e825908ca3d9afb5f2135ca8
nvandensteen <1052934+nvdsteen@users.noreply.github.com>
2024-03-01 14:44:08 +0100

    [REF,FIX] data requests, long&lat missing

--- src/aums_data_request.py
+++ src/aums_data_request.py
@@ -1,6 +1,6 @@
 from copy import deepcopy
 from pathlib import Path
-from typing import Literal
+from typing import List, Literal
 
 import hydra
 import matplotlib.pyplot as plt
@@ -130,29 +130,13 @@ def get_unique_value_series(series):
         return pd.NA
 
 
-@hydra.main(
-    config_path="../conf", config_name="config_aums_request.yaml", version_base="1.2"
-)
-def main(cfg):
-    stapy.config.filename = Path(get_original_cwd()).joinpath("outputs/.stapy.ini")
-    stapy.set_sta_url(cfg.data_api.base_url)
-
-    filter_obs_cfg = filter_cfg_to_query(cfg.data_api.filter)
-
-    filter_ds_cfg = datastream_id_in_list_filter_conditions(
-        cfg.data_api.filter.datastreams
-    )
-    query = get_results_specified_datastreams_query(
-        cfg.data_api.things.id,
-        filter_condition_observations=filter_obs_cfg,
-        filter_conditions_datastreams=filter_ds_cfg,
-    )
-
-    response = get_query_response(query)
-    df = response_datastreams_to_df(response)
+def time_conversions(df: pd.DataFrame) -> pd.DataFrame:
     df[Df.TIME + "_round"] = df[Df.TIME].dt.round("1s")
     df["dt"] = np.abs((df[Df.TIME] - df[Df.TIME + "_round"]).dt.total_seconds())
+    return df
 
+
+def wrapper_pivot_df(df: pd.DataFrame) -> pd.DataFrame:
     pivoted = df.pivot(
         index=[Df.TIME + "_round", "dt", Df.LAT, Df.LONG, Df.IOT_ID],
         columns=[
@@ -164,22 +148,33 @@ def main(cfg):
         ],
         values=[Df.QC_FLAG, Df.RESULT],
     )
-    cq = pivoted.columns[pivoted.columns.get_level_values(0).isin([Df.QC_FLAG])]
-    pivoted[cq] = pivoted[cq].fillna(9).map(QualityFlags)
+    return pivoted
+
+
+def get_flag_columns(df: pd.DataFrame) -> pd.Index:
+    cq = df.columns[df.columns.get_level_values(0).isin([Df.QC_FLAG])]
+    return cq
+
+
+def get_agg_data_from_pivoted(
+    pivoted: pd.DataFrame, flag_columns: pd.Index | List[str]
+) -> pd.DataFrame:
+    pivoted[flag_columns] = pivoted[flag_columns].fillna(9).map(QualityFlags)
     datastreams = pivoted.columns.get_level_values(Df.DATASTREAM_ID).unique()
 
     pivoted = pivoted.droplevel(Df.IOT_ID)
     df_out = pd.DataFrame()
-    df_out_coordinates = df_c = (
+    df_out_coordinates = (
         pivoted.reset_index([Df.LAT, Df.LONG])[[Df.LAT, Df.LONG]]
         .sort_values([Df.TIME + "_round", "dt"])
         .groupby(Df.TIME + "_round")
         .first()
     )
     pivoted = pivoted.drop(index=[Df.LAT, Df.LONG])
-    column_time_round = tuple([Df.TIME + "_round"] + ["" for i in pivoted.columns.levels[1:]]) # type: ignore
+    column_time_round = tuple([Df.TIME + "_round"] + ["" for i in pivoted.columns.levels[1:]])  # type: ignore
     pivoted.to_csv("raw_data.csv")
     df_out = pd.DataFrame()
+    # df_out = deepcopy(df_out_coordinates)
     for ds_i in datastreams:
         df_i = pivoted[
             pivoted.columns[pivoted.columns.get_level_values(Df.DATASTREAM_ID) == ds_i]
@@ -202,7 +197,44 @@ def main(cfg):
     df_out = df_out.sort_index(axis=1, level=0)
     df_out.columns = df_out.columns.swaplevel(0, 1)  # type: ignore
     df_out = df_out.sort_index(axis=1, level=0)
+    df_out = pd.concat([df_out_coordinates, df_out], axis=1, sort=False)
+
     df_out.index.name = Df.TIME
+    return df_out
+
+
+def get_agg_from_response(response: dict) -> pd.DataFrame:
+    df = response_datastreams_to_df(response)
+    df = time_conversions(df)
+
+    pivoted = wrapper_pivot_df(df)
+    cq = get_flag_columns(pivoted)
+    df_out = get_agg_data_from_pivoted(pivoted, flag_columns=cq)
+
+    return df_out
+
+
+@hydra.main(
+    config_path="../conf", config_name="config_aums_request.yaml", version_base="1.2"
+)
+def main(cfg):
+    stapy.config.filename = Path(get_original_cwd()).joinpath("outputs/.stapy.ini")
+    stapy.set_sta_url(cfg.data_api.base_url)
+
+    filter_obs_cfg = filter_cfg_to_query(cfg.data_api.filter)
+
+    filter_ds_cfg = datastream_id_in_list_filter_conditions(
+        cfg.data_api.filter.datastreams
+    )
+    query = get_results_specified_datastreams_query(
+        cfg.data_api.things.id,
+        filter_condition_observations=filter_obs_cfg,
+        filter_conditions_datastreams=filter_ds_cfg,
+    )
+
+    response = get_query_response(query)
+    df_out = get_agg_from_response(response)
+
     df_out.to_csv(cfg.csv_file)
 
 
