cd49750c3d76a94ed43b316eedd2a6c775189c47
nvandensteen <1052934+nvdsteen@users.noreply.github.com>
2024-03-05 08:55:13 +0100

    [TST,REF]

--- tests/test_queries.py
+++ tests/test_queries.py
@@ -1,35 +1,19 @@
 import json
 
-import pandas as pd
-import pandas.testing as pdt
 import pytest
-from pandas.api import types
-
 # from numpy.testing import
-from test_utils import cfg, mock_response, mock_response_full, mock_response_full_obs
+from test_utils import (cfg, mock_response, mock_response_full,
+                        mock_response_full_obs)
 
-from models.enums import Df, Entities, Properties, QualityFlags
+from models.enums import Df, Entities
 from services.config import QCconf, filter_cfg_to_query
-from services.df import response_obs_to_df, response_single_datastream_to_df
 from services.qc import CAT_TYPE
-from services.requests import (
-    build_query_datastreams,
-    build_query_observations,
-    get_nb_datastreams_of_thing,
-    get_observations_count_thing_query,
-    get_request,
-    get_results_n_datastreams,
-    get_results_n_datastreams_query,
-    response_datastreams_to_df,
-)
-
-# from services.df import response_datastreams_to_df
-#
-# class TestApi:
-#     @pytest.mark.usefixtures("mock_response")
-#     def test_get_id_result_lists(self, mock_response):
-#         a = get_id_result_lists(0)
-#         assert 1 == 1
+from services.requests import (build_query_datastreams,
+                               get_nb_datastreams_of_thing,
+                               get_observations_count_thing_query, get_request,
+                               get_results_n_datastreams,
+                               get_results_n_datastreams_query,
+                               response_datastreams_to_df)
 
 
 class TestServicesConfig:
@@ -157,53 +141,3 @@ class TestServicesRequests:
         df = response_datastreams_to_df(res)
 
         assert "Not all observations are extracted!" in caplog.text
-
-
-class TestDf:
-    @pytest.mark.skip(reason="no features in fixture at the moment & should be moved!")
-    def test_features_request_to_df(self):
-        assert False
-
-    def test_response_single_datastream_to_df_qc_flag(self):
-        with open(
-            "./tests/resources/single_datastream_response_missing_resultquality.json"
-        ) as f:
-            res = json.load(f)
-        df = response_single_datastream_to_df(res)
-        assert not df.isnull().any().any()
-        pdt.assert_series_equal(
-            df[Df.QC_FLAG],
-            pd.Series([2, 0, 2, 2, 0]).apply(QualityFlags).astype(CAT_TYPE),  # type: ignore
-            check_names=False,
-        )
-
-    # @pytest.mark.skip(reason="fails after including qc flag in df, missing from json")
-    def test_features_datastreams_request_to_df(self, mock_response_full):
-        response_in = get_results_n_datastreams("random")[1]
-        datastreams_data = response_in[Entities.DATASTREAMS]
-        df = response_single_datastream_to_df(datastreams_data[1])
-        assert not Entities.FEATUREOFINTEREST in df.keys()
-
-    # incomplete!
-    def test_shape_observations_request_to_df(self, mock_response_full_obs):
-        response_in = get_request("random")[1]
-        df = response_obs_to_df(response_in)
-        assert df.shape == (10, 6)
-
-    # incomplete comparison
-    # @pytest.mark.skip(reason="fails  AND not used")
-    def test_shape_datastreams_request_to_df(self, mock_response_full):
-        response_in = get_results_n_datastreams("random")[1]
-        df = response_datastreams_to_df(response_in)
-        assert df.shape == (27, 11)
-
-    @pytest.mark.skip(reason="fails after including qc flag")
-    def test_num_dtypes_datastreams_request_to_df(self, mock_response_full):
-        response_in = get_results_n_datastreams("random")[1]
-        datastreams_data = response_in[Entities.DATASTREAMS]
-        df = response_single_datastream_to_df(datastreams_data[1])
-
-        assert all(
-            types.is_numeric_dtype(df[ci])
-            for ci in [Properties.IOT_ID, Df.RESULT, Df.DATASTREAM_ID, Df.LONG, Df.LAT]
-        )
