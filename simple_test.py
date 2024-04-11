2f714b35bac758b1d288a88fc69347ba9650eeda
Nils Van den Steen <1052934+nvdsteen@users.noreply.github.com>
2024-04-11 21:57:24 +0200

    [ADD,WIP] shiny

--- simple_test.py
+++ simple_test.py
@@ -10,31 +10,44 @@ import matplotlib.pyplot as plt
 
 penguins = load_penguins()
 
-df = csv_to_df("SUBSET.csv").sort_values(Df.TIME)
+# df = csv_to_df("SUBSET.csv").sort_values(Df.TIME)
 df = csv_to_df("testing_dataset.csv").sort_values(Df.TIME)
 
+df = df.drop(df.loc[df[Df.RESULT]<=0.3].index)
 df = calc_zscore_results(df, Df.DATASTREAM_ID, rolling_time_window="60min")
-df = df.loc[df[Df.DATASTREAM_ID] == 7770]
+
+# df = df.loc[df[Df.DATASTREAM_ID] == 7770]
+# df = df.loc[df[Df.DATASTREAM_ID] == 7850]
+df = df.loc[df[Df.DATASTREAM_ID].isin([7849, 7795])]
+# df = df.drop(df.loc[df[Df.RESULT] <= 10.].index)
+df = df.sort_values(Df.ZSCORE)
 df["RANGE"] = 0
+
+min_r2 = round(df.loc[df[Df.DATASTREAM_ID] == 7849, Df.RESULT].min(), 2)
+max_r2 = round(df.loc[df[Df.DATASTREAM_ID] == 7849, Df.RESULT].max(), 2)
+
 
 #SET CATEGORY RANGE
 
 ui.input_slider("zscore_abs_limit", "absolute zscore limit", 1, 100, 20)
-ui.input_slider("max", "max", df[Df.RESULT].min(), df[Df.RESULT].max()*1.1, df[Df.RESULT].max()*1.1)
-ui.input_slider("min", "min", df[Df.RESULT].min(), df[Df.RESULT].max()*1.1, df[Df.RESULT].min()*1.1)
+ui.input_slider("max", "max", round(min_r2*0.9, 2), round(max_r2*1.1, 2), round(max_r2*1.1, 2))
+ui.input_slider("min", "min", round(min_r2*0.9, 2), round(max_r2*1.1, 2), round(min_r2*0.9, 2))
 
 @render.plot(alt="Results plot with zscore filter indication")  
 def plot():  
     # ax = sns.scatterplot(data=penguins, x="body_mass_g", y="bill_length_mm")  
     df["RANGE"] = 0
     df.loc[df[Df.ZSCORE].abs() > input.zscore_abs_limit(), "RANGE"] = 1
-    plt.scatter(df[Df.TIME].values, df[Df.RESULT].values, c="b", s=1)
-    df_one = df.loc[df["RANGE"] == 1]
-    plt.scatter(df_one[Df.TIME].values, df_one[Df.RESULT].values, c="r")
-    ax = plt.gca() 
-    ax.set_ylim([input.min(), input.max()])
+    # plt.scatter(df[Df.TIME].values, df[Df.RESULT].values, c="b", s=1)
+    # df_one = df.loc[df["RANGE"] == 1]
+    # plt.scatter(df_one[Df.TIME].values, df_one[Df.RESULT].values, c="r")
+    # ax = plt.gca() 
+    # ax.set_ylim([input.min(), input.max()])
+    fig, ax = plt.subplots(1, 2)
+    sns.scatterplot(data=df.loc[df[Df.DATASTREAM_ID] == 7849], x=Df.TIME, y=Df.RESULT, hue="RANGE", linewidth=0, ax=ax[0], legend=False)  
+    sns.scatterplot(data=df.loc[df[Df.DATASTREAM_ID] == 7795], x=Df.TIME, y=Df.RESULT, hue="RANGE", linewidth=0, ax=ax[1], legend=False)  
     # ax = sns.scatterplot(data=df, x=Df.TIME, y=Df.RESULT, hue="RANGE", linewidth=0)  
     # ax.set_title("Palmer Penguins")
     # ax.set_xlabel("Mass (g)")
     # ax.set_ylabel("Count")
-    return ax
+    return fig
