d818ee741ef82023aa939aacca497980c4931606
nvandensteen <1052934+nvdsteen@users.noreply.github.com>
2024-04-16 11:35:51 +0200

    [FIX] time comparisons

--- crontab/qc.sh
+++ crontab/qc.sh
@@ -8,6 +8,7 @@ ROOT_DIR=$1
 source $ROOT_DIR/crontab/functions.sh
 
 START_MESSAGE="Start QC script."
+MACHINE_TZ=$(date +"%Z")
 
 mkdir -p $ROOT_DIR/outputs
 source $ROOT_DIR/crontab/env_qc_settings
@@ -18,10 +19,10 @@ cd $ROOT_DIR
 # get timestamp latest execution QC
 
 QC_LOG=$ROOT_DIR/cron_out
-PATTERN_TIME_QC_LOG="\[\([0-9]\{4\}-[0-9]\{2\}-[0-9]\{2\} [0-9]\{2\}:[0-9]\{2\}:[0-9]\{2\}\)\].*"
+PATTERN_TIME_QC_LOG="\[\([0-9]\{4\}-[0-9]\{2\}-[0-9]\{2\} [0-9]\{2\}:[0-9]\{2\}:[0-9]\{2\}\)\].*" # OF THE ENTRY, LOCAL TZ!
 GREP_OUT_QC=$(grep_last_occurrences "$QC_LOG" "$START_MESSAGE" "1")
 TIME_PREVIOUS_QC=$(parse_date "$GREP_OUT_QC" "$PATTERN_TIME_QC_LOG")
-TIME_PREVIOUS_QC=${TIME_PREVIOUS_QC:-$(date -u -d "$TIME_PREVIOUS_QC UTC -10minutes" "$FMT")}
+TIME_PREVIOUS_QC=${TIME_PREVIOUS_QC:-$(date -d "$TIME_PREVIOUS_QC $MACHINE_TZ -10minutes" "$FMT")}
 
 print_current_time
 echo $START_MESSAGE
@@ -58,31 +59,31 @@ while read -r LINE; do
         START_I=$(get_date "$END_I UTC -$((DT_INT+OVERLAP))$DT_UNIT")
         echo "Starting docker container for range $START_I - $END_I"
 
-        # start container to do QC
-        CONTAINER_ID=$(docker run \
-                -d --rm --network=host --user "$(id -u):$(id -g)"\
-                --workdir /app \
-                -v "$CONFIG_FOLDER":/app/conf \
-                -v "$OUTPUT_FOLDER":/app/outputs \
-                -e DEV_SENSORS_USER="$SENSORS_USER" \
-                -e DEV_SENSORS_PASS="$SENSORS_PASS" \
-                rbinsbmdc/quality_assurance_tool:$IMAGE_TAG \
-                "time.start=$START_I" "time.end=$END_I")
+        # # start container to do QC
+        # CONTAINER_ID=$(docker run \
+        #         -d --rm --network=host --user "$(id -u):$(id -g)"\
+        #         --workdir /app \
+        #         -v "$CONFIG_FOLDER":/app/conf \
+        #         -v "$OUTPUT_FOLDER":/app/outputs \
+        #         -e DEV_SENSORS_USER="$SENSORS_USER" \
+        #         -e DEV_SENSORS_PASS="$SENSORS_PASS" \
+        #         rbinsbmdc/quality_assurance_tool:$IMAGE_TAG \
+        #         "time.start=$START_I" "time.end=$END_I")
         
-        STATUS_CODE_CONTAINER="$(docker container wait $CONTAINER_ID)"
-        print_current_time
-        echo "Status code $CONTAINER_ID: $STATUS_CODE_CONTAINER"
+        # STATUS_CODE_CONTAINER="$(docker container wait $CONTAINER_ID)"
+        # print_current_time
+        # echo "Status code $CONTAINER_ID: $STATUS_CODE_CONTAINER"
 
-        # variables needed for transfer script
-        FMT_TRANSFER_SCRIPT="+%Y-%m-%d %H:%M:%S"
-        startdate=$(date -u --date="$START_I UTC" "$FMT_TRANSFER_SCRIPT")
-        enddate=$(date -u --date="$END_I UTC" "$FMT_TRANSFER_SCRIPT")
+        # # variables needed for transfer script
+        # FMT_TRANSFER_SCRIPT="+%Y-%m-%d %H:%M:%S"
+        # startdate=$(date -u --date="$START_I UTC" "$FMT_TRANSFER_SCRIPT")
+        # enddate=$(date -u --date="$END_I UTC" "$FMT_TRANSFER_SCRIPT")
         
-        print_current_time
-        echo "Start production "
+        # print_current_time
+        # echo "Start production "
         
-        # run transfer script as other user
-        sudo -i -u ndeville startdate="$startdate" enddate="$enddate"/usr/bin/env bash $ROOT_DIR/crontab/sta_raw_to_sta_prod_transfer\ 1.sh
+        # # run transfer script as other user
+        # sudo -i -u ndeville startdate="$startdate" enddate="$enddate"/usr/bin/env bash $ROOT_DIR/crontab/sta_raw_to_sta_prod_transfer\ 1.sh
         COUNTER=$((COUNTER+1))
     fi
 done <<< "$GREP_OUT_TRANSF"
