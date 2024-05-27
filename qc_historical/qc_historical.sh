ef526874abe3a2a0008a8bcca52804f39b5382fd
nvandensteen <1052934+nvdsteen@users.noreply.github.com>
2024-05-27 16:30:37 +0200

    [WIP] config corrupted

--- qc_historical/qc_historical.sh
+++ qc_historical/qc_historical.sh
@@ -4,6 +4,7 @@ IMAGE_TAG="v0.7.0"
 CONFIG_NAME="config.yaml"
 
 # 
+# ./qc_historical.sh -s "2023-05-24 09:30:00" -e "2023-05-24 10:30:00" -d "60min" -o "10" -i "tmp" -c "config.yaml" -t >> qc_historical_$(date "+%Y%m%d").log 2>&1
 
 # Function to display usage
 usage() {
@@ -12,7 +13,7 @@ usage() {
 }
 
 # Parse arguments
-while getopts ":s:e:d:o:i:c:" opt; do
+while getopts ":s:e:d:o:i:c:t" opt; do
     case $opt in
         s) START="$OPTARG"
         ;;
@@ -26,8 +27,10 @@ while getopts ":s:e:d:o:i:c:" opt; do
         ;;
         c) CONFIG_NAME="$OPTARG"
         ;;
+        t) MODE="_testing"
+        ;;
         \?) echo "Invalid option -$OPTARG" >&2
-            usage
+           usage
         ;;
         :) echo "Option -$OPTARG requires an argument." >&2
            usage
@@ -40,12 +43,12 @@ if [ -z "$START" ] || [ -z "$END" ] || [ -z "$TOTAL_TIME_WINDOW" ] || [ -z "$WIN
     usage
 fi
 
-source ./env_hist
-source ./.env
+source ./env_hist$MODE
+source ./.env$MODE
 
 TIME_WINDOW_INT=$(echo $TOTAL_TIME_WINDOW | tr -dc '0-9')
 TIME_WINDOW_INT=$(($TIME_WINDOW_INT))
-TIME_WINDOW_UNIT=$(printf '%s\n' "$TOTAL_TIME_WINDOW//[[:digit:]]/}")
+TIME_WINDOW_UNIT=$(printf '%s\n' "${TOTAL_TIME_WINDOW//[[:digit:]]/}")
 
 START_SECONDS=$(date -u --date="$START" +%s)
 END_SECONDS=$(date -u --date="$END" +%s)
@@ -59,9 +62,11 @@ do
     CONFIG_FOLDER=$CONFIG_FOLDER_HIST
     OUTPUT_FOLDER=$OUTPUT_FOLDER_HIST
 
-    START_II=$(date -u --date="$START_I UTC +""$((-WINDOW_OVERLAP))""$DT_UNIT" "+%Y-%m-%d %H:%M")
+    START_II=$(date -u --date="$START_I UTC +""$((-WINDOW_OVERLAP))""$TIME_WINDOW_UNIT" "+%Y-%m-%d %H:%M")
 
     END_I=$(date -u --date="$START_I UTC" "+%Y-%m-%d %H:%M")
+    echo $CONFIG_FOLDER
+    echo "$OUTPUT_FOLDER":/app/outputs
 
     docker run \
         -it --rm --network=host --user "$(id -u):$(id -g)"\
@@ -72,7 +77,8 @@ do
         -e DEV_SENSORS_PASS="$SENSORS_PASS" \
         rbinsbmdc/quality_assurance_tool:$IMAGE_TAG \
         "--config-name" $CONFIG_NAME \
-        "time.start=$START_II" "time.end=$END_I"
+        "time.start=$START_II" "time.end=$END_I" \
+        "data_api.base_url=$BASE_URL"
 
     START_I=$END_I
  
