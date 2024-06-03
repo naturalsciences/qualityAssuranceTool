5b691c783aa2253c560580f5d19d70c0c503fac9
nvds <1052934+nvdsteen@users.noreply.github.com>
2024-06-03 16:43:20 +0200

    [ENH] historical qc

--- qc_historical/qc_historical.sh
+++ qc_historical/qc_historical.sh
@@ -2,6 +2,7 @@
 
 IMAGE_TAG="v0.7.0"
 CONFIG_NAME="config.yaml"
+DATE_TIME_FORMAT="+%Y-%m-%d %H:%M:%S"
 
 # 
 # ./qc_historical.sh -s "2023-05-24 09:30:00" -e "2023-05-24 10:30:00" -d "60min" -o "10" -i "tmp" -c "config.yaml" -t >> qc_historical_$(date "+%Y%m%d").log 2>&1
@@ -43,6 +44,18 @@ if [ -z "$START" ] || [ -z "$END" ] || [ -z "$TOTAL_TIME_WINDOW" ] || [ -z "$WIN
     usage
 fi
 
+echo "----------------------------------------------------------"
+echo "start qc_historical.sh script."
+echo "Parameters:"
+echo "Start: $START"
+echo "End: $END"
+echo "TOTAL_TIME_WINDOW: $TOTAL_TIME_WINDOW"
+echo "WINDOW_OVERLAP: $WINDOW_OVERLAP"
+echo "IMAGE_TAG: $IMAGE_TAG"
+echo "CONFIG_NAME: $CONFIG_NAME"
+echo "MODE: $MODE"
+echo "---------------"
+
 source ./env_hist$MODE
 source ./.env$MODE
 
@@ -62,11 +75,9 @@ do
     CONFIG_FOLDER=$CONFIG_FOLDER_HIST
     OUTPUT_FOLDER=$OUTPUT_FOLDER_HIST
 
-    START_II=$(date -u --date="$START_I UTC +""$((-WINDOW_OVERLAP))""$TIME_WINDOW_UNIT" "+%Y-%m-%d %H:%M")
+    START_II=$(date -u --date="$START_I UTC +""$((-WINDOW_OVERLAP))""$TIME_WINDOW_UNIT" "$DATE_TIME_FORMAT")
 
-    END_I=$(date -u --date="$START_I UTC" "+%Y-%m-%d %H:%M")
-    echo $CONFIG_FOLDER
-    echo "$OUTPUT_FOLDER":/app/outputs
+    END_I=$(date -u --date="$START_I UTC + ""$TOTAL_TIME_WINDOW" "$DATE_TIME_FORMAT")
 
     docker run \
         -it --rm --network=host --user "$(id -u):$(id -g)"\
